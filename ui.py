5ae9b1d30bd30d6de4674f1da1fca16623ba6e05
Veronika2310 <136489628+Veronika2310@users.noreply.github.com>
2024-02-15 12:47:36 +0100

    Add files via upload

---
+++ ui.py
@@ -0,0 +1,9 @@
+
+
+def interface():
+    print("Добрый  день! Вы попали на сайт! \n 1-запись данных \n 2 - вывод данных")
+    command = int(input("Введите число"))
+
+    while command != 1 and command !=2:
+        print("Неправильный ввод")
+command = int(input("Введите число"))
